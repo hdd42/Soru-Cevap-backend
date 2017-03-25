571e9b219b59f0c1b17ff3200872906eeebc48ab
huseyin <hdonmez@medocity.com>
2017-03-25 03:04:13 -0400

    votes count

--- controllers/statusCtrl.js
+++ controllers/statusCtrl.js
@@ -1,9 +1,16 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+const Question = mongoose.model('Question');
+const Category = mongoose.model('Category');
+const User = mongoose.model('User');
 const router = express.Router()
 
 module.exports = (app) => {
     app.use('/api/status', (req,res,next) =>{
+
+        Category.remove({}).then(_R => console.log(_R))
+        User.remove({}).then(_R => console.log(_R))
+        Question.remove({}).then(_R => console.log(_R))
         res.status(200).json({
             success:0,
             message:'Api Up and runnig'
